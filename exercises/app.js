7ecbae78a1ce8524d5f579083fdad57fe5b3abf5
Konstantin Ostrovskii <grinv@outlook.com>
2023-02-25 19:23:08 +0800

    4-5-timer-notifications

--- exercises/app.js
+++ exercises/app.js
@@ -2,7 +2,8 @@ const calc = require("./4-1-calc/index.js");
 const calcEvents = require("./4-1-calc-event/index.js");
 const timer = require("./4-2-timer/index.js");
 const multithreads = require("./4-4-multithreads/index.js");
+const timerNotifications = require("./4-5-timer-notifications");
 
-const app = multithreads;
+const app = timerNotifications;
 
 app();
